73f8ce2304ef03d0d654c9c436e485e2b83e7d28
Navid Boloorian <navidboloorian0@gmail.com>
2024-04-14 10:26:31 -0700

    added borders to form

--- src/contact_form/ContactForm.tsx
+++ src/contact_form/ContactForm.tsx
@@ -5,11 +5,14 @@ const ContactForm = () => {
         <div className={`content ${styles.formWrapper}`}>
             <h2>Contact</h2>
             <form>
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Company" />
-                <input type="email" placeholder="Email" />
-                <input type="text" placeholder="Number" />
-                <textarea placeholder="Message" rows={10}></textarea>
+                <div className={styles.inputs}>
+                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Company" />
+                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Number" />
+                    <textarea placeholder="Message" rows={10}></textarea>
+                </div>
+                <input type="submit"/>
             </form>
         </div>
     );
